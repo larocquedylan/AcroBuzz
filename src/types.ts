bab208cdaa3487c0636673051cb0d2ac3aad9075
Dylan L <dylancblarocque@gmail.com>
2023-04-05 16:16:11 -0700

    access req and res cookie in session middleware

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
 import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core';
+import { Request, Response } from 'express'; // access req and res in resolvers
 
 export type myContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request;
+  res: Response;
 };
