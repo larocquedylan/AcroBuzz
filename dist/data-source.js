2752fc822b60c1b25157f6c5cc2e5cd0eb050419
Dylan L <dylancblarocque@gmail.com>
2023-04-10 09:50:44 -0700

    typeorm config

--- dist/data-source.js
+++ dist/data-source.js
@@ -3,20 +3,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.myDataSource = void 0;
 const Post_1 = require("./entities/Post");
 const User_1 = require("./entities/User");
 const typeorm_1 = require("typeorm");
 const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
-const myDataSource = new typeorm_1.DataSource({
+exports.myDataSource = new typeorm_1.DataSource({
     type: 'postgres',
     database: 'acrobuzz2',
     url: process.env.DATABASE_URL,
+    host: 'localhost',
+    port: 5432,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     synchronize: true,
     logging: true,
     entities: [Post_1.Post, User_1.User],
+    migrations: {
+        path: path_1.default.join(__dirname, './migrations'),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
 });
-exports.default = myDataSource;
 //# sourceMappingURL=data-source.js.map
--- src/data-source.ts
+++ src/data-source.ts
@@ -3,21 +3,23 @@ import { User } from './entities/User';
 import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
 import { __prod__ } from './consts';
+import path from 'path';
 
 dotenv.config();
 
-const myDataSource = new DataSource({
+export const myDataSource = new DataSource({
   type: 'postgres',
   database: 'acrobuzz2',
   url: process.env.DATABASE_URL,
-  // host: 'localhost',
-  // port: 5432,
+  host: 'localhost',
+  port: 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   synchronize: true, // __prod__, true in production
   logging: true, // !__prod__, true in development //  creates tables auto without migrations
   entities: [Post, User],
-  // migrations: ['src/migrations/**/*.ts'],
+  migrations: {
+    path: path.join(__dirname, './migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  } as any,
 });
-
-export default myDataSource;
