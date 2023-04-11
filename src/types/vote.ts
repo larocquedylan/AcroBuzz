eb073bd4b6dd0d90f3d0da872747f233be07d50f
Dylan L <dylancblarocque@gmail.com>
2023-04-11 12:36:55 -0700

    Create vote graphql schema

---
+++ src/types/vote.ts
@@ -0,0 +1,24 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+import { PostType } from './post';
+import { UserType } from './user';
+
+@ObjectType()
+export class VoteType {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  voteType: string;
+
+  @Field(() => Int)
+  postId: number;
+
+  @Field(() => PostType)
+  post: PostType;
+
+  @Field(() => Int)
+  userId: number;
+
+  @Field(() => UserType)
+  user: UserType;
+}
