4c849db7ca88b9294ccf3ed0f61738f55b538d45
Dylan L <dylancblarocque@gmail.com>
2023-04-19 12:55:25 -0700

    remove redis from context

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express'; // access req and res in resolvers
 import { Session } from 'express-session'; // access session in resolvers
-// import { Redis } from 'ioredis'; // access redis in resolvers
-import { RedisStore } from 'connect-redis'; // Import RedisStore
 
 export type myContext = {
   req: Request & { session: Session & { userId: number } };
   res: Response;
   prisma: PrismaClient;
-  redis: RedisStore;
 };
