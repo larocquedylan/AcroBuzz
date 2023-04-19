8cdddfcc4a5532338d651b735f739b4d1035d17e
Dylan L <dylancblarocque@gmail.com>
2023-04-18 17:24:30 -0700

    server crashing ?

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import { PrismaClient } from '@prisma/client';
+import 'dotenv-safe/config';
 import { COOKIE_NAME, __prod__ } from './consts';
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { buildSchema } from 'type-graphql';
-import { HelloResolver } from './resolvers/hello';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { expressMiddleware } from '@apollo/server/express4';
-import { PostResolver } from './resolvers/post';
 import 'reflect-metadata';
 import { UserResolver } from './resolvers/user';
-import RedisStore from 'connect-redis';
-import session from 'express-session';
-import { createClient } from 'redis';
-import { myContext } from './types';
 import { VoteResolver } from './resolvers/voter';
+import { PostResolver } from './resolvers/post';
+import { HelloResolver } from './resolvers/hello';
+
+import session from 'express-session';
+import connectRedis, { Client } from 'connect-redis';
+import Redis from 'ioredis';
+
+import { myContext } from './types';
 
 const main = async () => {
   const prisma = new PrismaClient();
@@ -22,14 +25,11 @@ const main = async () => {
 
   const app = express();
 
-  // Initialize client.
-  let redisClient = createClient();
-  redisClient.connect().catch(console.error);
+  const redisClient = new Redis(process.env.REDIS_URL);
 
-  // Initialize store.
-  let redisStore = new RedisStore({
-    client: redisClient,
-    prefix: 'myapp:',
+  const RedisStore = connectRedis(session);
+  const sessionStoreRedis = new RedisStore({
+    client: redisClient as unknown as Client,
     disableTouch: true,
   });
 
@@ -53,16 +53,16 @@ const main = async () => {
     }),
     bodyParser.json(),
     session({
-      store: redisStore,
+      store: sessionStoreRedis,
       name: COOKIE_NAME,
-      resave: false, // required: force lightweight session keep alive (touch)
-      saveUninitialized: false, // recommended: only save session when data exists
-      secret: 'garypayton', // use a env later
+      resave: false,
+      saveUninitialized: false,
+      secret: 'garypayton',
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 5, // 5 years
-        httpOnly: true, // cookie only accessible by the web server
-        sameSite: 'lax', // protection against CSRF
-        secure: __prod__, // cookie only works in https, when prod is true
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: __prod__,
       },
     }),
     expressMiddleware(apolloServer, {
@@ -73,17 +73,107 @@ const main = async () => {
           prisma,
           req: req as express.Request & { session: typeof session },
           res,
-          redis: redisClient,
+          redis: sessionStoreRedis,
         };
       },
     })
   );
 
-  app.listen(8080, () => {
-    console.log('Server started on localhost:8080');
+  app.listen(parseInt(process.env.PORT), () => {
+    console.log('Server started on localhost:4000');
   });
 };
 
 main().catch((err) => {
   console.error(err);
 });
+
+// import { PrismaClient } from '@prisma/client';
+// import 'dotenv-safe/config';
+// import { COOKIE_NAME, __prod__ } from './consts';
+// import express from 'express';
+// import { ApolloServer } from '@apollo/server';
+// import { buildSchema } from 'type-graphql';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import { expressMiddleware } from '@apollo/server/express4';
+// import 'reflect-metadata';
+// import { UserResolver } from './resolvers/user';
+// import { VoteResolver } from './resolvers/voter';
+// import { PostResolver } from './resolvers/post';
+// import { HelloResolver } from './resolvers/hello';
+
+// // import RedisStore from 'connect-redis';
+// import session from 'express-session';
+// import { Redis } from 'ioredis';
+// // import { redis } from './redis';
+
+// import { myContext } from './types';
+// import connectRedis from 'connect-redis';
+
+// const main = async () => {
+//   const prisma = new PrismaClient();
+//   console.log('Connected to the PostgreSQL database');
+
+//   const app = express();
+
+//   const redis = new Redis(process.env.REDIS_URL);
+//   const RedisStore = connectRedis(session);
+
+//   app.get('/', (_, res) => {
+//     res.send('hello');
+//   });
+
+//   const apolloServer = new ApolloServer({
+//     schema: await buildSchema({
+//       resolvers: [HelloResolver, PostResolver, UserResolver, VoteResolver],
+//       validate: false,
+//     }),
+//   });
+
+//   await apolloServer.start();
+
+//   app.use(
+//     cors<cors.CorsRequest>({
+//       origin: ['http://localhost:3000'],
+//       credentials: true,
+//     }),
+//     bodyParser.json(),
+//     session({
+//       store: new RedisStore({
+//         client: redis,
+//         disableTouch: true,
+//       }),
+//       name: COOKIE_NAME,
+//       resave: false, // required: force lightweight session keep alive (touch)
+//       saveUninitialized: false, // recommended: only save session when data exists
+//       secret: 'garypayton', // use a env later
+//       cookie: {
+//         maxAge: 1000 * 60 * 60 * 24 * 365 * 5, // 5 years
+//         httpOnly: true, // cookie only accessible by the web server
+//         sameSite: 'lax', // protection against CSRF
+//         secure: __prod__, // cookie only works in https, when prod is true
+//       },
+//     }),
+//     expressMiddleware(apolloServer, {
+//       context: async ({ req, res }): Promise<myContext> => {
+//         const session = req.session as session.Session & { userId: number };
+
+//         return {
+//           prisma,
+//           req: req as express.Request & { session: typeof session },
+//           res,
+//           redis: redis,
+//         };
+//       },
+//     })
+//   );
+
+//   app.listen(parseInt(process.env.PORT), () => {
+//     console.log('Server started on localhost:4000');
+//   });
+// };
+
+// main().catch((err) => {
+//   console.error(err);
+// });
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express'; // access req and res in resolvers
 import { Session } from 'express-session'; // access session in resolvers
-import { Redis } from 'ioredis'; // access redis in resolvers
+// import { Redis } from 'ioredis'; // access redis in resolvers
+import { RedisStore } from 'connect-redis'; // Import RedisStore
 
 export type myContext = {
   req: Request & { session: Session & { userId: number } };
   res: Response;
   prisma: PrismaClient;
-  redis: Redis;
+  redis: RedisStore;
 };
